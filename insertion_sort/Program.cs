c8cf221b9a082df7291a2e245a8663ab94e74608
Jair Guerrero <jair.guerrero@overactive.com>
2022-11-04 00:03:48 -0500

    Agregar el código al repositorio

---
+++ insertion_sort/Program.cs
@@ -0,0 +1,31 @@
+﻿internal class Program
+{
+  private static void Main(string[] args)
+  {
+    int[] arr = { 8, 3, 9, 5, 0, 1, 5 };
+    InsertionSort(arr);
+    PrintArray(arr);
+  }
+
+  static void InsertionSort(int[] arr)
+  {
+    for (int i = 1; i < arr.Length; i++)
+    {
+      int key = arr[i];
+      int j = i - 1;
+
+      while (j >= 0 && arr[j] > key)
+      {
+        arr[j + 1] = arr[j];
+        j = j - 1;
+      }
+      arr[j + 1] = key;
+    }
+  }
+
+  static void PrintArray(int[] arr)
+  {
+    for (int i = 0; i < arr.Length; i++)
+      Console.Write(arr[i] + " ");
+  }
+}
